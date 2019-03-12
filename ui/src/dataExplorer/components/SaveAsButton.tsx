bc8f60e30f611f402a05384a524000ffc84c8df6
Brandon Farmer <bthesorceror@gmail.com>
2019-03-12 14:58:21 -0700

    Data Explorer save as overlay uses router

--- ui/src/dataExplorer/components/SaveAsButton.tsx
+++ ui/src/dataExplorer/components/SaveAsButton.tsx
@@ -1,36 +1,17 @@
 // Libraries
 import React, {PureComponent} from 'react'
+import {withRouter, WithRouterProps} from 'react-router'
 
 // Components
-import SaveAsCellForm from 'src/dataExplorer/components/SaveAsCellForm'
-import SaveAsTaskForm from 'src/dataExplorer/components/SaveAsTaskForm'
 import {IconFont, Button, ComponentColor} from '@influxdata/clockface'
-import {Radio, Overlay} from 'src/clockface'
 
 // Styles
 import 'src/dataExplorer/components/SaveAsButton.scss'
 
-enum SaveAsOption {
-  Dashboard = 'dashboard',
-  Task = 'task',
-}
+interface Props extends WithRouterProps {}
 
-interface Props {}
-
-interface State {
-  isOverlayVisible: boolean
-  saveAsOption: SaveAsOption
-}
-
-class SaveAsButton extends PureComponent<Props, State> {
-  public state: State = {
-    isOverlayVisible: false,
-    saveAsOption: SaveAsOption.Dashboard,
-  }
-
+class SaveAsButton extends PureComponent<Props, {}> {
   public render() {
-    const {isOverlayVisible, saveAsOption} = this.state
-
     return (
       <>
         <Button
@@ -40,62 +21,17 @@ class SaveAsButton extends PureComponent<Props, State> {
           color={ComponentColor.Primary}
           titleText="Save your query as a Dashboard Cell or a Task"
         />
-        <Overlay visible={isOverlayVisible}>
-          <Overlay.Container maxWidth={600}>
-            <Overlay.Heading
-              title="Save As"
-              onDismiss={this.handleHideOverlay}
-            />
-            <Overlay.Body>
-              <div className="save-as--options">
-                <Radio>
-                  <Radio.Button
-                    active={saveAsOption === SaveAsOption.Dashboard}
-                    value={SaveAsOption.Dashboard}
-                    onClick={this.handleSetSaveAsOption}
-                    data-testid="cell-radio-button"
-                  >
-                    Dashboard Cell
-                  </Radio.Button>
-                  <Radio.Button
-                    active={saveAsOption === SaveAsOption.Task}
-                    value={SaveAsOption.Task}
-                    onClick={this.handleSetSaveAsOption}
-                    data-testid="task-radio-button"
-                  >
-                    Task
-                  </Radio.Button>
-                </Radio>
-              </div>
-              {this.saveAsForm}
-            </Overlay.Body>
-          </Overlay.Container>
-        </Overlay>
       </>
     )
   }
 
-  private get saveAsForm(): JSX.Element {
-    const {saveAsOption} = this.state
-
-    if (saveAsOption === SaveAsOption.Dashboard) {
-      return <SaveAsCellForm dismiss={this.handleHideOverlay} />
-    } else if (saveAsOption === SaveAsOption.Task) {
-      return <SaveAsTaskForm dismiss={this.handleHideOverlay} />
-    }
-  }
-
   private handleShowOverlay = () => {
-    this.setState({isOverlayVisible: true})
-  }
+    const {
+      location: {pathname},
+    } = this.props
 
-  private handleHideOverlay = () => {
-    this.setState({isOverlayVisible: false})
-  }
-
-  private handleSetSaveAsOption = (saveAsOption: SaveAsOption) => {
-    this.setState({saveAsOption})
+    this.props.router.push(`${pathname}/save`)
   }
 }
 
-export default SaveAsButton
+export default withRouter<{}, {}>(SaveAsButton)
